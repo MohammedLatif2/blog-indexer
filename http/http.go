0518f6605a4f8ca5bfaff2134b5b7b79a1ef3e3e
Mohammed Alsayed <mohammed2250@gmail.com>
2018-03-20 11:32:58 +0300

    refactored error handling + changed delete signature

--- http/http.go
+++ http/http.go
@@ -7,22 +7,39 @@ import (
 	"github.com/MohammedLatif2/blog-indexer/elastic"
 )
 
-func SearchHandler(w http.ResponseWriter, r *http.Request) {
-	el := elastic.NewElastic("http://localhost:9200/rayed/posts/")
+type Server struct {
+	el *elastic.Elastic
+}
+
+func NewServer(el *elastic.Elastic) *Server {
+	return &Server{el}
+}
+
+func (server *Server) MakeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r)
+	}
+}
+
+func (server *Server) SearchHandler(w http.ResponseWriter, r *http.Request) {
 	query := r.FormValue("q")
-	result, _ := el.Search(query)
+	result, err := server.el.Search(query)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(result)
 }
 
-func StatsHandler(w http.ResponseWriter, r *http.Request) {
+func (server *Server) StatsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Not implemented yet"))
 }
 
-func StartWebServer() {
+func (server *Server) Start() {
 	log.Println("Starting Web Server")
-	http.HandleFunc("/search", SearchHandler)
-	http.HandleFunc("/stats", StatsHandler)
+	http.HandleFunc("/search", server.MakeHandler(server.SearchHandler))
+	http.HandleFunc("/stats", server.MakeHandler(server.StatsHandler))
 	// http.HandleFunc("/delete", DeleteHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
